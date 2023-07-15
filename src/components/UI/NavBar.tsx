e288988ebaa02ce8b0d5b78fbc6249bf2c1719f4
Pashs-ba <pashs.ba@gmail.com>
2023-07-15 16:46:53 +0300

    add move home

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router-dom";
+
 export default function NavBar({title, bootstrap_icon_name}: { title: string, bootstrap_icon_name?: string }) {
     return (
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><i className={`bi ${bootstrap_icon_name}`}/>{title}</a>
+                <Link className="navbar-brand" to="/"><i className={`bi ${bootstrap_icon_name}`}/>{title}</Link>
             </div>
         </nav>
     )
